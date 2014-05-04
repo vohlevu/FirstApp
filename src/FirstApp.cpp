db5f46fee266d213128e1965880a9a27fa18170b
Vu Vo <vohlevu@gmail.com>
2014-05-04 20:48:25 +0700

    output to string and color text

--- src/FirstApp.cpp
+++ src/FirstApp.cpp
@@ -20,8 +20,23 @@
  *
  ***************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define KNRM  "\033[0m"
+#define KRED  "\033[31m"
+#define KGRN  "\033[32m"
+#define KYEL  "\033[33m"
+#define KBLU  "\033[34m"
+#define KMAG  "\033[35m"
+#define KCYN  "\033[36m"
+#define KWHT  "\033[37m"
 #include <curl/curl.h>
-
+struct string {
+  char *ptr;
+  size_t len;
+};
+void init_string(struct string *s);
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s);
 int main(void)
 {
   CURL *curl;
@@ -29,7 +44,12 @@ int main(void)
 
   curl = curl_easy_init();
   if(curl) {
+	  struct string s;
+	  init_string(&s);
+
     curl_easy_setopt(curl, CURLOPT_URL, "http://google.com.vn");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
     /* example.com is redirected, so we tell libcurl to follow redirection */
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
@@ -42,6 +62,32 @@ int main(void)
 
     /* always cleanup */
     curl_easy_cleanup(curl);
+    printf("\n%s'%s'%s'Text Normal'\n", KGRN, s.ptr, KNRM);
   }
+ // system("read");
   return 0;
+}
+
+void init_string(struct string *s) {
+  s->len = 0;
+  s->ptr = (char*)malloc(s->len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  s->ptr[0] = '\0';
+}
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+  size_t new_len = s->len + size*nmemb;
+  s->ptr = (char*)realloc(s->ptr, new_len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr+s->len, ptr, size*nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
+
+  return size*nmemb;
 }
